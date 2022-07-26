76386f3add7e384cb218bbfd17adac5294515cf1
dr2 <novikovxkv@gmail.com>
2022-07-26 06:53:44 +0300

    задача 1 - разворот списка

---
+++ task_1/reverse.php
@@ -0,0 +1,31 @@
+<?php
+
+class Test {
+    public $next;
+}
+
+$a = new Test();
+$b = new Test();
+$c = new Test();
+$d = new Test();
+$a->next = $b;
+$b->next = $c;
+$c->next = $d;
+$d->next = null;
+
+//функция  разворота списка
+function reverse(Test $a, object $result = null) : object {
+
+    $new = new Test();
+    $new->next = $result;
+    $result = $new;
+
+    if ($a->next !== null) {
+        $result = reverse($a->next, $result);
+    }
+
+    return $result;
+}
+
+$ob1 = reverse($a);
+var_dump($ob1);
